e070ebd544c6d3f0df72b9410191d13fd9a673c3
John Freeman <jfreeman08@gmail.com>
2020-01-09 11:18:14 -0600

    Incorporate ed25519 key prefix

--- tests/test_key_pair.py
+++ tests/test_key_pair.py
@@ -7,17 +7,26 @@ from xpring.key_pair import KeyPair
 def test_ed25519_key_pair():
     encoded_seed = 'sEdSKaCy2JT7JaM7v95H9SxkhP9wS2r'
     key_pair = KeyPair.from_encoded_seed(encoded_seed)
-    assert key_pair.public_key.hex().upper(
-    ) == '01FA53FA5A7E77798F882ECE20B1ABC00BB358A9E55A202D0D0676BD0CE37A63'
     assert key_pair.private_key.hex().upper(
-    ) == 'B4C4E046826BD26190D09715FC31F4E6A728204EADD112905B08B14B7F15C4F3'
+    ) == 'EDB4C4E046826BD26190D09715FC31F4E6A728204EADD112905B08B14B7F15C4F3'
+    assert key_pair.public_key.hex().upper(
+    ) == 'ED01FA53FA5A7E77798F882ECE20B1ABC00BB358A9E55A202D0D0676BD0CE37A63'
 
 
-@pytest.mark.skip
 def test_ed25519_address():
     encoded_seed = 'sEdSKaCy2JT7JaM7v95H9SxkhP9wS2r'
     key_pair = KeyPair.from_encoded_seed(encoded_seed)
     assert key_pair.address == 'rLUEXYuLiQptky37CqLcm9USQpPiz5rkpD'
+
+
+def test_ed25519_sign():
+    encoded_seed = 'sEdSKaCy2JT7JaM7v95H9SxkhP9wS2r'
+    key_pair = KeyPair.from_encoded_seed(encoded_seed)
+    message = b'test message'
+    signature = bytes.fromhex(
+        'CB199E1BFD4E3DAA105E4832EEDFA36413E1F44205E4EFB9E27E826044C21E3E2E848BBC8195E8959BADF887599B7310AD1B7047EF11B682E0D068F73749750E'
+    )
+    assert key_pair.sign(message) == signature
 
 
 @pytest.mark.skip
--- xpring/algorithms/ed25519.py
+++ xpring/algorithms/ed25519.py
@@ -7,23 +7,32 @@ from xpring import hashes
 from xpring.algorithms.signing import Seed, PrivateKey, PublicKey, Signature
 
 SEED_PREFIX = b'\x01\xE1\x4B'
-
-KEY_PREFIX = 'ED'
+"""
+For some reason, the team wanted public keys to be a uniform 33 bytes.
+ed25519 public keys are only 32 bytes, so they are given a 1-byte prefix.
+The private keys are as well, but just so they can be easily distinguished.
+"""
+KEY_PREFIX = b'\xED'
 
 
 # https://xrpl.org/cryptographic-keys.html#ed25519-key-derivation
 def derive_key_pair(seed: Seed) -> t.Tuple[PrivateKey, PublicKey]:
     private_key = hashes.sha512half(seed)
     public_key = nacl.signing.SigningKey(private_key).verify_key._key[:32]  # pylint: disable=protected-access
-    return (t.cast(PrivateKey, private_key), t.cast(PublicKey, public_key))
+    return (
+        t.cast(PrivateKey, KEY_PREFIX + private_key),
+        t.cast(PublicKey, KEY_PREFIX + public_key)
+    )
 
 
 def sign(message: bytes, private_key: PrivateKey) -> Signature:
-    key = nacl.signing.SigningKey(private_key)
+    assert private_key.startswith(KEY_PREFIX)
+    key = nacl.signing.SigningKey(private_key[len(KEY_PREFIX):])
     signature = key.sign(message, nacl.encoding.RawEncoder).signature
     return t.cast(Signature, signature)
 
 
 def verify(message: bytes, signature: Signature, public_key: PublicKey) -> bool:
-    key = nacl.signing.VerifyKey(public_key)
+    assert public_key.startswith(KEY_PREFIX)
+    key = nacl.signing.VerifyKey(public_key[len(KEY_PREFIX):])
     return key.verify(message, signature, nacl.encoding.RawEncoder) == message
