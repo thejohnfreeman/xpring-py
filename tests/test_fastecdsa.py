7bf04055ab3c5782e8999ac1418af5f4fa6558d1
John Freeman <jfreeman08@gmail.com>
2020-01-16 10:41:17 -0600

    Pass message bytes instead of hex to fastecdsa.sign

--- tests/test_fastecdsa.py
+++ tests/test_fastecdsa.py
@@ -10,7 +10,7 @@ def make_private_key(private_key_bytes: bytes) -> t.Any:
 
 def sign(private_key: t.Any, message_hash_bytes: bytes) -> bytes:
     r, s = ecdsa.sign(
-        message_hash_bytes.hex(),
+        message_hash_bytes,
         private_key,
         curve=curve.secp256k1,
         prehashed=True,
