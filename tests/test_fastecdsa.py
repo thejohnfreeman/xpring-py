f7b8562fd40848c18f4fbc6ff1e60b234d1970bf
John Freeman <jfreeman08@gmail.com>
2020-01-16 10:36:17 -0600

    Use fastecdsa's DER encoder

--- tests/test_fastecdsa.py
+++ tests/test_fastecdsa.py
@@ -1,20 +1,7 @@
 import typing as t
 
 from fastecdsa import curve, ecdsa
-
-
-def encode_der(r: int, s: int) -> bytes:
-    assert r < (1 << (32 * 8))
-    assert s < (1 << (32 * 8))
-    # If r and s are 32-byte integers, the DER encoding is:
-    # 1-byte code for SEQUENCE (0x30) +
-    # 1-byte length of sequence (0x01 + 0x01 + 0x20 + 0x01 + 0x01 + 0x20 = 0x44) +
-    # 1-byte code for INTEGER (0x02) + 1-byte for length of r (0x20) + r
-    # 1-byte code for INTEGER (0x02) + 1-byte for length of s (0x20) + s
-    # https://docs.microsoft.com/en-us/windows/win32/seccertenroll/about-introduction-to-asn-1-syntax-and-encoding
-    r_bytes = r.to_bytes(32, byteorder='big')
-    s_bytes = s.to_bytes(32, byteorder='big')
-    return b'\x30\x44' + b'\x02\x20' + r_bytes + b'\x02\x20' + s_bytes
+from fastecdsa.encoding.der import DEREncoder
 
 
 def make_private_key(private_key_bytes: bytes) -> t.Any:
@@ -28,4 +15,4 @@ def sign(private_key: t.Any, message_hash_bytes: bytes) -> bytes:
         curve=curve.secp256k1,
         prehashed=True,
     )
-    return encode_der(r, s)
+    return DEREncoder.encode_signature(r, s)
