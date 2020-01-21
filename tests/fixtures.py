2e7c2a8be1cb798ead7747406f3f69889920617e
John Freeman <jfreeman08@gmail.com>
2020-01-21 12:11:42 -0600

    Add tests for ed25519

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -7,7 +7,7 @@ from xpring import hashes
 
 SECP256K1_SIGNATURE_EXAMPLES = (
     ('signing_key_hex', 'message_digest_hex', 'signature_hex'),
-    [
+    (
         # https://github.com/btccom/secp256k1-go/blob/f7178bcde5780809c6f819d62c3e7a37c635e749/secp256k1/sign_vectors.yaml#L3-L5
         pytest.param(
             '31a84594060e103f5a63eb742bd46cf5f5900d8406e2726dedfc61c7cf43ebad',
@@ -23,13 +23,37 @@ SECP256K1_SIGNATURE_EXAMPLES = (
             id='39dfc6',
         ),
         # https://github.com/ripple/ripple-keypairs/blob/master/test/fixtures/api.json#L2-L11
-        (
+        pytest.param(
             'd78b9735c3f26501c7337b8a5727fd53a6efdbc6aa55984f098488561f985e23',
             # sha512half(b'test message')
             '950b2a7effa78f51a63515ec45e03ecebe50ef2f1c41e69629b50778f11bc080',
             '30440220583a91c95e54e6a651c47bec22744e0b101e2c4060e7b08f6341657dad9bc3ee02207d1489c7395db0188d3a56a977ecba54b36fa9371b40319655b1b4429e33ef2d',
+            id='ripple-keypairs',
         ),
-    ]
+    )
+)
+
+ED25519_SIGNATURE_EXAMPLES = (
+    ('signing_key_hex', 'message_hex', 'signature_hex'),
+    (
+        # https://ed25519.cr.yp.to/python/sign.input
+        # To understand these vectors, one must read the code:
+        # https://ed25519.cr.yp.to/python/sign.py
+        # This is the format:
+        # signing key + verifying key : verifying key : message : signature + message
+        # Don't ask me why the verifying key and message are needlessly
+        # repeated in concatentations.
+        (
+            '9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60',
+            '',
+            'e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b',
+        ),
+        (
+            '3b26516fb3dc88eb181b9ed73f0bcd52bcd6b4c788e4bcaf46057fd078bee073',
+            '4284abc51bb67235',
+            'd6addec5afb0528ac17bb178d3e7f2887f9adbb1ad16e110545ef3bc57f9de2314a5c8388f723b8907be0f3ac90c6259bbe885ecc17645df3db7d488f805fa08',
+        ),
+    )
 )
 
 
---
+++ tests/test_ed25519.py
@@ -0,0 +1,17 @@
+import nacl.signing
+import pytest
+
+from .fixtures import ED25519_SIGNATURE_EXAMPLES
+
+
+@pytest.mark.parametrize(*ED25519_SIGNATURE_EXAMPLES)
+def test_sign(
+    signing_key_hex: str,
+    message_hex: str,
+    signature_hex: str,
+):
+    signing_key_bytes = bytes.fromhex(signing_key_hex)
+    assert len(signing_key_bytes) == 32
+    signing_key = nacl.signing.SigningKey(signing_key_bytes)
+    signature = signing_key.sign(bytes.fromhex(message_hex)).signature
+    assert signature.hex() == signature_hex
