704d3927ccf81b94a9ee35c2ed88d104c4c576f3
Mayukha Vadari <mvadari@ripple.com>
2022-02-28 18:03:13 -0500

    remove xrpl.js folder

--- submodules/xrpl.js/packages/xrpl/test/integration/serverUrl.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable node/no-process-env -- needed to find standalone connection */
-const HOST = process.env.HOST ?? '0.0.0.0'
-const PORT = process.env.PORT ?? '6006'
-const serverUrl = `ws://${HOST}:${PORT}`
-
-export default serverUrl
